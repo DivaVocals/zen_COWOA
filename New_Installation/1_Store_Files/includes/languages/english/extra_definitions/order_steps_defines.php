b8e317f1a8007ce2f750b48fac86a6db1ac27ee2
DivaVocals <divavocals@hotmail.com>
2013-12-29 19:25:09 -0800

    Upload v2.4 files
    Update master to match the version of COWOA active when this repository
    was created.

---
+++ New_Installation/1_Store_Files/includes/languages/english/extra_definitions/order_steps_defines.php
@@ -0,0 +1,17 @@
+<?php
+// $Id: order_steps_defines.php $ 
+// $Id: Integrated COWOA v2.4  - 2007 - 2013
+//
+define('ORDER_STEPS_IMAGE', 'arrow_checkout.gif');
+define('ORDER_STEPS_IMAGE_HEIGHT', '24');
+define('ORDER_STEPS_IMAGE_WIDTH', '24');
+define('ORDER_STEPS_IMAGE_ALT', 'Current Checkout Step');
+
+define('TEXT_ORDER_STEPS_BILLING', 'Billing');
+define('TEXT_ORDER_STEPS_1', 'Shipping');
+define('TEXT_ORDER_STEPS_2', 'Payment');
+define('TEXT_ORDER_STEPS_3', 'Order Review');
+define('TEXT_ORDER_STEPS_4', 'Order Complete');
+
+define('ORDER_REVIEW', '<h4 class="confirm">PLEASE REVIEW &amp; CONFIRM YOUR ORDER!!</h4>');
+// eof
