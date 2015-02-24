c985b0b24993e2d7ad7bbc18f8ae7479ba04ab94
DivaVocals <divawebdzgn@aol.com>
2015-02-24 10:39:28 -0800

    Minor changes to the Font Awesome Loaders

--- New_Installation/3_ZenCart_1.5.3/YOUR_ADMIN/includes/functions/extra_functions/init_font_awesome.php
+++ New_Installation/3_ZenCart_1.5.3/YOUR_ADMIN/includes/functions/extra_functions/init_font_awesome.php
@@ -1,6 +1,7 @@
 <?php
 // -----
 // Add the font-awesome controls to Configuration->My Store, in support of CSS3 Buttons v1.1.0 and later.
+// Adapted for COWOA by Copyright (C) 2015, Over the Hill Web Consulting (C Jones)
 //
 if (!defined ('FONT_AWESOME_INCLUDE_FONT_AWESOME_CSS')) {
   $db->Execute ("INSERT INTO " . TABLE_CONFIGURATION . " ( configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, use_function, set_function ) VALUES ( 'Include a link to the Font Awesome stylesheet?', 'FONT_AWESOME_INCLUDE_FONT_AWESOME_CSS', 'false', 'If true, a link to the Font Awesome CSS file will be included in each page\'s header.  If your template already includes Font Awesome, leave this setting <em>false</em>.', 19, 148, NULL , 'zen_cfg_select_option(array(\'true\', \'false\'),')");
--- New_Installation/3_ZenCart_1.5.3/includes/templates/YOUR_TEMPLATE/common/tpl_load_font_awesome.php
+++ New_Installation/3_ZenCart_1.5.3/includes/templates/YOUR_TEMPLATE/common/tpl_load_font_awesome.php
@@ -1,6 +1,7 @@
 <?php
 // -----
 // Part of the CSS3 buttons for Zen Cart plugin, v1.1.0 and later.  Copyright (C) 2014, Vinos de Frutas Tropicales (lat9).
+// Adapted for COWOA by Copyright (C) 2015, Over the Hill Web Consulting (C Jones)
 //
 if (defined ('FONT_AWESOME_INCLUDE_FONT_AWESOME_CSS_VERSION') && defined ('FONT_AWESOME_INCLUDE_FONT_AWESOME_CSS_AWESOME') && FONT_AWESOME_INCLUDE_FONT_AWESOME_CSS_AWESOME == 'true') {
   echo '<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/' . FONT_AWESOME_INCLUDE_FONT_AWESOME_CSS_VERSION . '/css/font-awesome.min.css" />' . "\n";
